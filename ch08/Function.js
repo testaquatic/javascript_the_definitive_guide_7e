0c34a133d3f8e0347e93d67d5a4b5971d2d17013
aquatictest <aquatictest@proton.me>
2025-09-21 14:12:21 +0900

    독서 종료

--- ch08/Function.js
+++
@@ -1,9 +0,0 @@
-// 지금 버전에서는 실행이 되지 않는다.
-let scope = "global";
-function constructFunction() {
-    let scope = "local";
-    return new Function( "return scope;");
-}
-
-let result = constructFunction();
-console.log(result());
